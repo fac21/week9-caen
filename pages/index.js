b61a67931db7ec984c67151de0c95cf374f56f5b
Amy Kuner <amykuner1@gmail.com>
2021-05-19 16:23:02 +0100

    adding cards to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import layoutStyles from "../components/layout.module.css";
 import Link from "next/link";
 
 import getAllProducts from "../database/model";
@@ -10,30 +11,20 @@ import getAllProducts from "../database/model";
 import Layout from "../components/layout";
 import Basket from "./basket";
 
-export async function getStaticProps({ req, res }) {
-  const allProducts = await getAllProducts();
-  //console.log(allProducts);
-  const productData = JSON.stringify(allProducts);
-  //console.log(productData);
-  return {
-    props: { productData },
-  };
-}
-
 //Cheeses product data will be populated at build time by getStaticProps()
 function CheeseCard({ productData }) {
-  console.log(productData);
-  //const productsArr = JSON.parse(productData);
-  //console.log(productsArr);
-  //console.log(productsArr);
+  const dataObject = JSON.parse(productData.productData);
+  console.log(dataObject);
   return (
     <>
-      {/* {allCheeses.map((cheese) => ( */}
-      <a href="`./${productData.name}`" className={styles.card}>
-        <h2></h2>
-        <p></p>
-      </a>
-      {/* ))} */}
+      {dataObject.map((cheese) => {
+        return (
+          <a href={"./" + cheese.name} className={layoutStyles.card}>
+            {cheese.image}
+            <h2>{cheese.name}</h2>
+          </a>
+        );
+      })}
     </>
   );
 }
@@ -57,58 +48,12 @@ export default function Home(productData) {
         </Link>
       </section>
 
-      <section id="products_grid" className={styles.grid}>
-        <CheeseCard />
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Non tellus
-          orci ac auctor augue mauris augue. Sit amet risus nullam eget felis.
-          Tempor commodo ullamcorper a lacus vestibulum sed. Eget est lorem
-          ipsum dolor sit amet consectetur. Ornare arcu odio ut sem nulla
-          pharetra diam sit. Volutpat blandit aliquam etiam erat velit
-          scelerisque. Habitant morbi tristique senectus et netus et malesuada
-          fames ac. Sit amet aliquam id diam maecenas ultricies mi eget. Amet
-          nulla facilisi morbi tempus. Nunc pulvinar sapien et ligula
-          ullamcorper malesuada proin libero. Ornare arcu odio ut sem nulla
-          pharetra diam sit amet. Ullamcorper a lacus vestibulum sed. Nibh cras
-          pulvinar mattis nunc sed blandit libero volutpat sed. Praesent semper
-          feugiat nibh sed pulvinar proin. Pellentesque adipiscing commodo elit
-          at imperdiet dui accumsan sit amet. Non blandit massa enim nec. Dis
-          parturient montes nascetur ridiculus mus mauris. Tincidunt vitae
-          semper quis lectus nulla at volutpat. Et ultrices neque ornare aenean.
-          Tincidunt ornare massa eget egestas purus viverra accumsan. Commodo
-          elit at imperdiet dui accumsan sit amet. Sapien eget mi proin sed.
-          Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper.
-          Fames ac turpis egestas sed. Posuere urna nec tincidunt praesent
-          semper feugiat. Quam viverra orci sagittis eu volutpat odio facilisis.
-          Porttitor massa id neque aliquam vestibulum morbi blandit. Varius vel
-          pharetra vel turpis nunc eget lorem. Ultrices vitae auctor eu augue ut
-          lectus arcu bibendum. In iaculis nunc sed augue lacus viverra.
-          Ultrices neque ornare aenean euismod. Cursus metus aliquam eleifend mi
-          in nulla posuere sollicitudin aliquam. Morbi tincidunt ornare massa
-          eget egestas purus viverra. Dictum varius duis at consectetur lorem
-          donec massa sapien. Amet volutpat consequat mauris nunc. Ligula
-          ullamcorper malesuada proin libero. Pretium nibh ipsum consequat nisl
-          vel pretium lectus quam id. Pellentesque elit eget gravida cum sociis.
-          Sodales ut eu sem integer vitae. Pretium quam vulputate dignissim
-          suspendisse in est ante in nibh. Risus commodo viverra maecenas
-          accumsan. Massa id neque aliquam vestibulum morbi blandit cursus risus
-          at. Nunc mattis enim ut tellus elementum sagittis vitae et. Consequat
-          id porta nibh venenatis cras sed felis eget. Vel elit scelerisque
-          mauris pellentesque pulvinar pellentesque habitant. A scelerisque
-          purus semper eget duis at tellus. Egestas sed sed risus pretium quam
-          vulputate. Nulla aliquet enim tortor at auctor urna nunc id cursus.
-          Etiam dignissim diam quis enim lobortis scelerisque fermentum dui.
-          Blandit massa enim nec dui nunc. Posuere lorem ipsum dolor sit amet
-          consectetur adipiscing. Integer vitae justo eget magna fermentum
-          iaculis. Venenatis urna cursus eget nunc. Sit amet luctus venenatis
-          lectus magna fringilla urna. Facilisis volutpat est velit egestas.
-          Tincidunt dui ut ornare lectus sit. Auctor neque vitae tempus quam
-          pellentesque nec nam aliquam. Est velit egestas dui id ornare arcu
-          odio ut sem. Metus dictum at tempor commodo ullamcorper a lacus
-          vestibulum. Augue interdum velit euismod in pellentesque massa
-          placerat duis ultricies. Facilisis gravida neque convallis a cras. Sit
-          amet massa vitae tortor condimentum lacinia.
+      <section id="products_grid" className={layoutStyles.product_grid}>
+        <div className={layoutStyles.filter_section}>
+          <h3>Filter</h3>
+        </div>
+        <div className={layoutStyles.cheese_cards}>
+          <CheeseCard productData={productData} />
         </div>
       </section>
 
@@ -127,4 +72,12 @@ export default function Home(productData) {
        </footer>  */}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allProducts = await getAllProducts();
+  const productData = JSON.stringify(allProducts);
+  return {
+    props: { productData },
+  };
 }
