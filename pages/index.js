12062009be428664d5bffab744024a151d418c85
epletnev <e.a.pletnev@gmail.com>
2021-05-20 17:27:50 +0100

    Added filter function
    Co-authored-by: chunzg <chunzhang53@gmail.com>"

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -13,31 +14,37 @@ import Basket from "./basket";
 import Login from "./login";
 
 //Cheeses product data will be populated at build time by getStaticProps()
-function CheeseCard({ productData }) {
+function CheeseCard({ productData, min, max, category }) {
   const dataObject = JSON.parse(productData.productData);
   console.log(dataObject);
   return (
     <>
       {/* src={`/assets/products/${prod.ProductName.replace(/ /g, "-")}.png`} */}
-      {dataObject.map((cheese) => {
-        return (
-          <a href={"./products/" + cheese.name} className={layoutStyles.card}>
-            <Image
-              src={`/${cheese.image}.jpeg`}
-              width="200"
-              height="170"
-              alt={cheese.name}
-            />
-            <h2>{cheese.name}</h2>
-            <p>See more</p>
-          </a>
-        );
-      })}
+      {dataObject
+        .filter((cheese) => category === "all" || cheese.category === category)
+        .filter((cheese) => cheese.price >= min && cheese.price <= max)
+        .map((cheese) => {
+          return (
+            <a href={"./products/" + cheese.name} className={layoutStyles.card}>
+              <Image
+                src={`/${cheese.image}.jpeg`}
+                width="200"
+                height="170"
+                alt={cheese.name}
+              />
+              <h2>{cheese.name}</h2>
+              <p>See more</p>
+            </a>
+          );
+        })}
     </>
   );
 }
 
 export default function Home(productData) {
+  const [min, setMin] = React.useState(0);
+  const [max, setMax] = React.useState(20);
+  const [category, setCategory] = React.useState("all");
   return (
     <Layout home>
       <section className={styles.landing}>
@@ -58,10 +65,77 @@ export default function Home(productData) {
 
       <section id="products_grid" className={layoutStyles.product_grid}>
         <div className={layoutStyles.filter_section}>
-          <h3>Filter</h3>
+          <fieldset>
+            <legend>Filter</legend>
+            <label htmlFor="min-price">
+              Min price
+              <input
+                type="range"
+                id="min-price"
+                min="1"
+                max="20"
+                step="1"
+                value={min}
+                onChange={(event) => setMin(event.target.value)}
+              />
+            </label>
+            <label htmlFor="max-price">
+              Max price
+              <input
+                type="range"
+                id="max-price"
+                min="1"
+                max="20"
+                step="1"
+                value={max}
+                onChange={(event) => setMax(event.target.value)}
+              />
+            </label>
+            <label htmlFor="all">
+              All
+              <input
+                type="radio"
+                name="categories"
+                id="all"
+                value="all"
+                checked={category === "all"}
+                onChange={(event) => setCategory(event.target.value)}
+              />
+            </label>
+            <label htmlFor="organic">
+              Organic
+              <input
+                type="radio"
+                name="categories"
+                id="organic"
+                value="organic"
+                checked={category === "organic"}
+                onChange={(event) => setCategory(event.target.value)}
+              />
+            </label>
+            <label htmlFor="vegetarian">
+              Vegetarian
+              <input
+                type="radio"
+                name="categories"
+                id="vegetarian"
+                value="vegetarian"
+                checked={category === "vegetarian"}
+                onChange={(event) => setCategory(event.target.value)}
+              />
+            </label>
+          </fieldset>
         </div>
         <div className={layoutStyles.cheese_cards}>
-          <CheeseCard productData={productData} />
+          <CheeseCard
+            productData={productData}
+            min={min}
+            setMin={setMin}
+            max={max}
+            setMax={setMax}
+            category={category}
+            setCategory={setCategory}
+          />
         </div>
       </section>
 
