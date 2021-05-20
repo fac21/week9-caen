1f38cc6fd4bfa7638cb035a5879193db16a140b3
chunzg <chunzhang53@gmail.com>
2021-05-20 12:19:28 +0100

    Fix individual product page
    Co-authored-by: Amy <amykuner1@gmail.com>

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -2,33 +2,40 @@ import Layout from "../../components/layout";
 import Head from "next/head";
 import styles from "../../components/layout.module.css";
 import { getAllProductNames } from "../../database/model";
+import { db } from "../../database/connection";
+import Link from "next/link";
 
-//returns an array of possible values for id
+//returns an array of possible values for name
 export async function getStaticPaths() {
   const allProductNames = await getAllProductNames();
-  console.log(allProductNames); // [{id: 1}, {id: 2}]
+  console.log(allProductNames); // [ { name: 'Baron Bigod Brie' }, { name: 'Chabichou de Poitou' } ]
   const productNameArray = allProductNames.map((item) => {
-    return { params: { name: item.name } };
+    return { params: { id: item.name } };
   });
+  // item.name.replace(/\s/g, "%20"
   console.log(productNameArray);
   return {
-    productNameArray,
+    paths: productNameArray,
     fallback: false,
   };
 }
 
+//Error: Invalid value returned from getStaticPaths in /products/[id]. Received undefined Expected: { paths: [], fallback: boolean }
+//See here for more info: https://nextjs.org/docs/messages/invalid-getstaticpaths-value
+
 // [ { params: { id: 'pre-rendering' } }, { params: { id: 'ssg-ssr' } } ]
 
-function getCheeseData() {
+function getCheeseData(id) {
+  console.log(typeof id); //Baron Bigod Brie
   return db
-    .query(`SELECT all FROM cheeses WHERE id = {id}`)
-    .then((result) => result.rows);
+    .query(`SELECT * FROM cheeses WHERE name = ($1)`, [id])
+    .then((result) => result.rows[0]);
 }
 
 //fetches necessary data for the cheese with id
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the cheese page using params.id
-  const cheeseData = await getcheeseData(params.id);
+  const cheeseData = await getCheeseData(params.id);
   return {
     props: {
       cheeseData,
@@ -45,8 +52,8 @@ export default function Cheese({ cheeseData }) {
         <img className={styles.product_image}>{cheeseData.image}</img>
         <p className={styles.product_description}>{cheeseData.description}</p>
         <p className={styles.product_price}>{cheeseData.price}</p>
-        <Link>
-          <a href="../basket">
+        <Link href="/basket">
+          <a>
             <p className={styles.add_to_basket}>{cheeseData.add_to_basket}</p>
           </a>
         </Link>
